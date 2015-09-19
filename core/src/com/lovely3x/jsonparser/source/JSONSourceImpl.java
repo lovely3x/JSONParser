81da1f37bd1f4406dc1bf75349e1d9d2601dd8f7
lovely3x <lovely3x@163.com>
2015-09-19 18:51:55 +0800

    修护了Unicode字符问题

--- core/src/com/lovely3x/jsonparser/source/JSONSourceImpl.java
+++ core/src/com/lovely3x/jsonparser/source/JSONSourceImpl.java
@@ -4,6 +4,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by lovely3x on 15-6-29.
@@ -27,8 +29,20 @@ public class JSONSourceImpl implements JSONSource {
      * @param string
      */
     public JSONSourceImpl(String string) {
-        this.input = string;
+        this(string, true);
     }
+
+
+    /**
+     * 使用一个字符串作为json输入源
+     *
+     * @param string
+     * @param unicodeDecode 是否使用unicode解码器解码
+     */
+    public JSONSourceImpl(String string, boolean unicodeDecode) {
+        this.input = unicodeDecode ? unicodeDecode(new StringBuilder(string)) : string;
+    }
+
 
     /**
      * 使用一个byte数组 作为json输入源 编码方式使用默认的UTF-8
@@ -52,7 +66,23 @@ public class JSONSourceImpl implements JSONSource {
      *               使用默认的编码方式 UTF-8
      */
     public JSONSourceImpl(InputStream stream) throws IOException {
-        this.input = streamToString(stream, DEFAULT_CHARSET);
+        this(stream, true);
+    }
+
+    /**
+     * @param stream 使用一个InputStream数组 作为json输入源
+     *               使用默认的编码方式 UTF-8
+     */
+    public JSONSourceImpl(InputStream stream, boolean unicodeDecode) throws IOException {
+        this.input = streamToString(stream, DEFAULT_CHARSET, unicodeDecode);
+    }
+
+    /**
+     * @param stream  使用一个InputStream数组 作为json输入源
+     * @param charset 使用指定的编码方式
+     */
+    public JSONSourceImpl(InputStream stream, String charset, boolean unicodeDecode) throws IOException {
+        this.input = streamToString(stream, charset, unicodeDecode);
     }
 
     /**
@@ -60,7 +90,7 @@ public class JSONSourceImpl implements JSONSource {
      * @param charset 使用指定的编码方式
      */
     public JSONSourceImpl(InputStream stream, String charset) throws IOException {
-        this.input = streamToString(stream, charset);
+        this.input = streamToString(stream, charset, true);
     }
 
     /**
@@ -70,7 +100,7 @@ public class JSONSourceImpl implements JSONSource {
      * @param charset 制定的字符编码集
      * @return 转换好的string
      */
-    private String streamToString(InputStream stream, String charset) throws IOException {
+    private String streamToString(InputStream stream, String charset, boolean unicodeDecode) throws IOException {
 
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         byte[] buf = new byte[1024 * 8];
@@ -81,8 +111,66 @@ public class JSONSourceImpl implements JSONSource {
         baos.flush();
         String str = new String(baos.toByteArray(), charset);
         baos.close();
-        return str;
+        return unicodeDecode ? unicodeDecode(new StringBuilder(str)) : str;
     }
+
+    public static final String unicodeDecode(StringBuilder sb) {
+        Pattern pattern = Pattern.compile("\\\\u.{4}");
+        Matcher matcher = pattern.matcher(sb.toString());
+        int len = 0;
+        while (matcher.find()) {
+            int start = matcher.start();
+            int end = matcher.end();
+            if (start != 0) {
+                if (sb.charAt(start - len - 1) != '\\') {
+                    CharSequence sub = sb.subSequence(start - len, end - len);
+                    sb.replace(start - len, end - len, String.valueOf(hexToChar(sub.toString())));
+                    len += 5;
+                }
+            } else {
+                CharSequence sub = sb.subSequence(start - len, end - len);
+                sb.replace(start - len, end - len, String.valueOf(hexToChar(sub.toString())));
+                len += 5;
+            }
+        }
+
+
+        String string = sb.toString();
+        char preChar = '\0';
+        int dividedCount = 0;
+
+        for (int i = 0; i < string.length(); i++) {
+            char currentChar = sb.charAt(i - dividedCount);
+            if (preChar == '\\') {
+                switch (currentChar) {
+                    case 'n':// \n
+                        sb.replace(i - dividedCount - 1, i - dividedCount + 1, "\n");
+                        dividedCount += 1;
+                        break;
+                    case 'r':// \r
+                        sb.replace(i - dividedCount - 1, i - dividedCount + 1, "\r");
+                        dividedCount += 1;
+                        break;
+                    case 't':// \t
+                        sb.replace(i - dividedCount - 1, i - dividedCount + 1, "\t");
+                        dividedCount += 1;
+                        break;
+                    case '\\':// \
+                        sb.replace(i - dividedCount - 1, i - dividedCount + 1, "\\");
+                        dividedCount += 1;
+                        break;
+                }
+            }
+            preChar = (preChar == '\\' && currentChar == '\\') ? '\0' : currentChar;
+        }
+
+        return sb.toString();
+    }
+
+    private static final char hexToChar(String hex) {
+        return (char) Integer.parseInt(hex.substring(2), 16);
+    }
+
 
     @Override
     public String input() {
